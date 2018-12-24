0ec1dcd5648291de4b64a2c1245656746525375f
Vidi <tangtaob@csair.com>
2018-12-24 11:22:14 +0800

    solution 937 commit

---
+++ src/com/vidi/demo/SolutionNo937.java
@@ -0,0 +1,28 @@
+package com.vidi.demo;
+
+/**
+ * @Author : Vidi
+ * @Date : 2018/12/24 8:24
+ * @Descripton :
+ * <p>
+ * Reorder Log Files
+ * <p>
+ * You have an array of logs.  Each log is a space delimited string of words.
+ * <p>
+ * For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+ * <p>
+ * Each word after the identifier will consist only of lowercase letters, or; Each word after the identifier will
+ * consist only of digits. We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that
+ * each log has at least one word after its identifier.
+ * <p>
+ * Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered
+ * lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in
+ * their original order.
+ * <p>
+ * Return the final order of the logs.
+ */
+public class SolutionNo937 {
+	public String[] reorderLogFiles(String[] logs) {
+		return null;
+	}
+}
