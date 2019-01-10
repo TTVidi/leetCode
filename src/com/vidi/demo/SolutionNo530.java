bda27f5d9e64aee9dd348c6cf1e13b5268ff2a2a
Vidi <tangtaob@csair.com>
2019-01-10 15:20:42 +0800

    solution 530 commit

---
+++ src/com/vidi/demo/SolutionNo530.java
@@ -0,0 +1,29 @@
+package com.vidi.demo;
+
+/**
+ * @Author : Vidi
+ * @Date : 2019/1/10 15:04
+ * @Descripton :
+ * <p>
+ * Minimum Absolute Difference in BST
+ * <p>
+ * Given a binary search tree with non-negative values, find the minimum absolute difference between values of any two
+ * nodes.
+ */
+public class SolutionNo530 {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+
+
+	public int getMinimumDifference(TreeNode root) {
+		return 0;
+	}
+}
